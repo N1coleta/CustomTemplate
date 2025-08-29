2b5b6aadc198c1f1cc91214db7416db9881958f9
Niki <132263016+N1coleta@users.noreply.github.com>
2025-08-30 00:32:41 +0300

    The basic c++ template

---
+++ BaseTemplate.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int>v;
+ifstream fin("");
+ofstream fout("");
+int main()
+{
+    int n;
+    fin >> n;
+    return 0;
+}
+//=^..^=
